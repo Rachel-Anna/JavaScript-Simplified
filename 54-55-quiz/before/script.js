8a57c09a52c28f34dcb7ce1d2ee1a7ac7066e804
Rochie424 <67237482+Rochie424@users.noreply.github.com>
2021-10-14 13:37:23 +0200

    adding

--- 54-55-quiz/before/script.js
+++ 54-55-quiz/before/script.js
@@ -1,16 +1,39 @@
-/*
-  TODO: 2. Select all elements needed
-    * The form element (has the id `quiz-form`)
-    * The answer inputs (have the class `answer`)
-    * BONUS: The questions (have the class `question-item`)
-    * BONUS: The alert (has the id `alert`)
-*/
+const FORM = document.querySelector("#quiz-form");
+const ANSWERS = Array.from(document.querySelectorAll(".answer"));
+const QUESTIONS = Array.from(document.querySelectorAll(".question-item"));
+const ALERT = document.querySelector("#alert");
+const BTN = document.querySelector("button");
 
-// TODO: 3. Create a submit event listener for the form that does the following.
-//    1. Prevent the default behaviour
-//    2. Get all selected answers (use the `checked` property on the input to determine if it is selected or not)
-//    3. Loop through the selected answer to see if they are correct or not (Check the value of the answer to see if it is the string "true")
-//    4. For each correct answer add the class `correct` to the parent with the class `question-item` and remove the class `incorrect`.
-//    5. For each incorrect answer add the class `incorrect` to the parent with the class `question-item` and remove the class `correct`.
-//    6. BONUS: Make sure unanswered questions show up as incorrect. The easiest way to do this is to add the incorrect class and removing the correct class from all question items before checking the correct answers
-//    7. BONUS: If all answers are correct show the element with the id `alert` and hide it after one second (look into setTimeout) (use the class active to show the alert and remove the class to hide it)
+let arr = [];
+ANSWERS.forEach((answer) => {
+  answer.addEventListener("click", (e) => {
+    if (!arr.includes(e.target.name)) {
+      arr.push(e.target.name);
+    }
+    if (arr.length === QUESTIONS.length) {
+      BTN.removeAttribute("disabled");
+    }
+  });
+});
+
+FORM.addEventListener("submit", (e) => {
+  e.preventDefault();
+  QUESTIONS.forEach((question) => {
+    question.classList.add("incorrect");
+    question.classList.remove("correct");
+  });
+  let correctAnswers = ANSWERS.filter(
+    (answer) => answer.checked && answer.value === "true"
+  );
+  correctAnswers.forEach((answer) => {
+    answer.closest(".question-item").classList.add("correct");
+    answer.closest(".question-item").classList.remove("incorrect");
+  });
+
+  if (correctAnswers.length === QUESTIONS.length) {
+    ALERT.classList.add("active");
+    setTimeout(() => {
+      ALERT.classList.remove("active");
+    }, 3000);
+  }
+});
